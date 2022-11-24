0c6d185c20412ea5b1c6e07e06414e21712f4850
Francisco Betancourt <betancourt.francisco@gmail.com>
2022-11-24 12:10:44 -0600

    Git remove registry credentials not needed

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,10 +24,3 @@ resource "digitalocean_container_registry" "registry" {
   subscription_tier_slug = "starter"
 }
 
-resource "digitalocean_container_registry_docker_credentials" "registry_credentials" {
-  registry_name = "pdfsvc-registry"
-  write         = true
-  depends_on = [
-    digitalocean_container_registry.registry
-  ]
-}
